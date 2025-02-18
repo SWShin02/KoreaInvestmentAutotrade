edb7b78d324887390854f9071eb5ace36cb0dd31
SWShin02 <69069253+SWShin02@users.noreply.github.com>
2025-02-18 19:39:18 +0900

    Add files via upload

---
+++ KR_AssetRebalancing.py
@@ -0,0 +1,15 @@
+from koreainvestmentAPI.Rebalancing import rebalancing
+
+if __name__ == "__main__":
+    base_filepath = "aggressive strategy"
+    main = rebalancing(f"{base_filepath}/config")
+    main.load_portfolio(f'{base_filepath}/portfolio/KR_Portfolio.csv')
+    main.get_asset_balance()
+    main.set_using_current()
+    main.make_trade_plan()
+    main.trade()
+    print("===========================")
+    print("자산 재분배가 완료되었습니다.")
+    print("===========================")
+    main.get_asset_balance()
+    main.get_cash_balance()
